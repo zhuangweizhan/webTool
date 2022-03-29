5fae23c1bf5d30b0567563965b1de0c4c8e158de
zhuangweizhan <365638983@qq.com>
2022-03-29 23:03:12 +0800

    分享前端自定义工具

--- routes/page.js
+++ routes/page.js
@@ -5,8 +5,58 @@ router.get('/', async (ctx, next) => {
     await ctx.render("index")
 });
 
+router.get('/page/aes', async (ctx, next) => {
+    await ctx.render("aes")
+});
+
 router.get('/page/babelMinToH5', async (ctx, next) => {
     await ctx.render("babelMinToH5")
 });
+
+router.get('/page/colorFormat', async (ctx, next) => {
+    await ctx.render("colorFormat")
+});
+
+router.get('/page/cssFormat', async (ctx, next) => {
+    await ctx.render("cssFormat")
+});
+
+router.get('/page/customerMessage', async (ctx, next) => {
+    await ctx.render("customerMessage")
+});
+
+
+router.get('/page/excelExport', async (ctx, next) => {
+    await ctx.render("excelExport")
+});
+
+router.get('/page/imageFormat', async (ctx, next) => {
+    await ctx.render("imageFormat")
+});
+
+router.get('/page/jsonFormat', async (ctx, next) => {
+    await ctx.render("jsonFormat")
+});
+
+router.get('/page/jsRun', async (ctx, next) => {
+    await ctx.render("jsRun")
+});
+
+router.get('/page/md5', async (ctx, next) => {
+    await ctx.render("md5")
+});
+
+router.get('/page/rsa', async (ctx, next) => {
+    await ctx.render("rsa")
+});
+
+router.get('/page/urlDecode', async (ctx, next) => {
+    await ctx.render("urlDecode")
+});
+
+router.get('/page/urlFormat', async (ctx, next) => {
+    await ctx.render("urlFormat")
+});
+
 
 module.exports = router;
